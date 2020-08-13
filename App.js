c179058471363bbc11f21cd995dc696827eb6a2a
jan lawrence <56373608+janhawk@users.noreply.github.com>
2020-08-13 17:08:04 +0200

    intro_react

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  Button,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// import Box from './components/Box';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <Text style={styles.top}>
+            I
+          </Text>
+          <Text style={styles.mid}>
+            LOVE
+          </Text>
+          <Text style={styles.bot}>
+            NUGGETS
+          </Text>
+        </ScrollView>
+        <Image source={require('./assets/konexio.png')} />
+        <Image
+        style={styles.konexio}
+        source={{
+          uri: 'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png',
+        }}
+      />
+        <Button
+        title="Press me"
+        color="red"
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 80,
+    backgroundColor: "beige",
   },
+  top: {
+    fontSize: 30,
+  },
+  mid: {
+    textAlign: "center",
+  },
+bot: {
+  fontWeight: "bold",
+},
+scrollView: {
+  backgroundColor: 'pink',
+  marginHorizontal: 20,
+},
+konexio: {
+  width: 150,
+  height: 50,
+},
+button: {
+ 
+  
+},
 });
+export default App;
---
+++ components/Box.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+  } from 'react-native';
+
+  class Box extends React.Component {
+      render() {
+          return(
+            <View style={StyleSheet.container}>
+
+            </View>
+          );
+      }
+  }
+  export default Box;
